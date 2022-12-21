7b81f81f0479cf0c6f7ea86af1b982e87cc44672
Ravi Bagdi <63995407+neilravi7@users.noreply.github.com>
2022-12-21 11:21:07 +0530

    update setting for allowed host

--- taleteller/settings.py
+++ taleteller/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['taleteller.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 
 
 # Application definition
