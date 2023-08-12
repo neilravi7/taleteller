630f4660b45aa6ea1632ae5735092cb7887c0ea7
Ravi Bagdi <ravibagdi799@outlook.com>
2023-08-12 14:29:25 +0530

    Improvize login an register form.

--- taleteller/settings.py
+++ taleteller/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
+ALLOWED_HOSTS = []
 
 
 # Application definition
